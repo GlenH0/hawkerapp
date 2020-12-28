4bbc2c359edc267881de902250f141d5de127e31
Glen Ho <47803471+GlenH0@users.noreply.github.com>
2020-12-29 03:41:24 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import * as Font from 'expo-font';
+import Food from './screens/food';
+import AppLoading from 'expo-app-loading';
+import  AppNavigator  from "./routes/drawer";
+import 'react-native-gesture-handler';
+
+const getFonts = () => Font.loadAsync({
+    'jetbrains-ital': require('./assets/fonts/JetBrainsMono-Italic-VariableFont_wght.ttf'),
+    'jetbrains-var': require('./assets/fonts/JetBrainsMono-VariableFont_wght.ttf'),
+    'latoR': require('./assets/fonts/Lato-Regular.ttf'),
+  });
+
+
+  export default function App() {
+    const [fontsLoaded, setFontsLoaded] = useState(false);
+  
+    if(fontsLoaded){
+      return (
+        <AppNavigator/>
+      );
+    }
+    else {
+      return (
+        <AppLoading
+        startAsync={getFonts}
+        onFinish={()=> setFontsLoaded(true)}
+        onError={console.warn}
+      />
+      );
+    }
+  
+  }
+
+
