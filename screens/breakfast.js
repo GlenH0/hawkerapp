1840cceb6cac1eff749503903138413a1c680a6c
Glen Ho <47803471+GlenH0@users.noreply.github.com>
2020-12-30 01:50:45 +0800

    Add files via upload

--- screens/breakfast.js
+++ screens/breakfast.js
@@ -1,37 +1,82 @@
-import React from 'react';
-import { StyleSheet, View, Text, Image } from "react-native";
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Image, FlatList, Dimensions, TouchableOpacity } from "react-native";
 // import {globalStyles, images} from '../styles/global';
 // import Card from '../shared/card';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import {BackHandler} from 'react-native';
 
+const dataList = [
+  {
+    key: '1', 
+    title:"Prata",
+    image: require('../assets/Breakfast/prata.png'),
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled'
+  }, 
+  {
+    key: '2', 
+    title:"hi",
+    text:'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.'
+  }, 
+  {key: '3'}, {key: '4'},
+  {key: '1', title:"hi"}, 
+  {key: '2'}, 
+  {key: '3'}, 
+  {key: '4'}  ]
 
-export default function Break({ route, navigation }) {
-  
-    return (
-      <View style={styles.view}>
-        <Text style={styles.text}>Breakfast Page</Text>
-      </View>
-    );
+const numColumns = 2
+const WIDTH = Dimensions.get("window").width;
+
+export default class App extends Component {
+
+    _renderItem = ({item, index}) => {
+      
+      return (
+        <View style={{flex:1}}>
+          <TouchableOpacity style={styles.itemStyle} onPress={() => this.props.navigation.navigate('break', item)}>
+            {/* <Text style={styles.itemText}>{item.key}</Text> */}
+            <Image style={styles.img} source={item.image}/>
+          </TouchableOpacity>
+          <Text style={{paddingLeft:10}}>{item.title}</Text>
+        </View>
+      )
+    }
+
+    render(){
+      return (
+        <View style={styles.container}>
+          <FlatList
+            data={dataList}
+            renderItem={this._renderItem}
+            keyExtractor={(item, index)=> index.toString()}
+            numColumns={numColumns}
+          />
+        </View>
+      );
+    }
   }
 
   const styles = StyleSheet.create({
-    rating: {
-      flexDirection: 'row',
-      justifyContent: 'center',
-      paddingTop: 16,
-      marginTop: 16,
-      borderTopWidth: 1,
-      borderTopColor: '#eee',
+    
+    container:{
+      flex: 1,
+      paddingTop: 40
     },
-    text:{
-      textAlign: 'center',
-      fontWeight: 'bold',
-      fontSize: 18,
-    },
-    view:{
-      justifyContent:'center',
+    itemStyle: {
+      backgroundColor: '#aeaeae',
       alignItems: 'center',
-      flex: 1
+      justifyContent: 'center',
+      height: 100,
+      flex: 1,
+      margin: 10,
+      height: WIDTH/numColumns
+    },
+    itemText: {
+      color: 'white',
+      fontSize: 30
+    },
+    img:{
+      resizeMode:'cover',
+      width: '100%',
+      height:'100%'
     }
   });
---
+++ screens/breakfastContent.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, View, Text, Image } from "react-native";
+// import {globalStyles, images} from '../styles/global';
+// import Card from '../shared/card';
+
+
+
+export default function Break({ route, navigation }) {
+    const { item, title, text, image, image1} = route.params;
+  
+    return (
+      <View>
+        <Image style={styles.img} source={image}/>
+        <View  style={styles.view}>
+            <Text style={styles.name}>{title}</Text>
+            <Text>Description</Text>
+            <Text>{text}</Text>   
+        </View>
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+   
+    view:{
+      flex: 1,
+      padding: 10
+    },
+    img:{
+        resizeMode:'cover',
+        width: '100%',
+        height: 350,
+    },
+    name:{
+        paddingBottom: 10,
+        fontWeight:'bold',
+        fontSize:32
+    }
+  });
