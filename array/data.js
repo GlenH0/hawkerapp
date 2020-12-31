e494afa3afeb6b2a19b530441df6295272a628ae
Glen Ho <47803471+GlenH0@users.noreply.github.com>
2020-12-31 23:43:25 +0800

    5

---
+++ array/data.js
@@ -0,0 +1,24 @@
+export 
+    const dataList = [
+        {
+          key: 'Prata', 
+          title:"Prata",
+          image: require('../assets/Breakfast/prata.png'),
+          text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled',
+          video: 'RSzITFbOtpQ',
+          rating: 4,
+          status: 'Halal'
+        }, 
+        {
+          key: 'Chew', 
+          title:"hi",
+          text:'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+          rating: 3,
+          status: 'All'
+        }, 
+        {key: 'Chicken'}, {key: '4',status: 'All'},
+        {key: '1', title:"hi",status: 'All'}, 
+        {key: '2', status: 'All'}, 
+        {key: '3', status: 'All'}, 
+        {key: '3', status: 'All'}, 
+          ];
