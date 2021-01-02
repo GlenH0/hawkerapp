34aadc16bbec531983628f38be7278de636fe365
Glen Ho <47803471+GlenH0@users.noreply.github.com>
2021-01-02 19:08:23 +0800

    6

--- styles/global.js
+++ styles/global.js
@@ -3,5 +3,16 @@ import {StyleSheet} from 'react-native';
 export const globalStyles = StyleSheet.create({
     container: {
         flex: 1,
+        
     },
 })
+
+export const images = {
+    ratings: {
+      '1': require('../assets/ratings/rating-1.png'),
+      '2': require('../assets/ratings/rating-2.png'),
+      '3': require('../assets/ratings/rating-3.png'),
+      '4': require('../assets/ratings/rating-4.png'),
+      '5': require('../assets/ratings/rating-5.png'),
+    }
+}
