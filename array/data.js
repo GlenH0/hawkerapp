c9f9e1548edd21eaba2cac173a502873b41c1b24
Glen Ho <47803471+GlenH0@users.noreply.github.com>
2021-01-04 19:50:55 +0800

    7

--- array/data.js
+++ array/data.js
@@ -1,24 +1,84 @@
 export 
     const dataList = [
         {
-          key: 'Prata', 
-          title:"Prata",
-          image: require('../assets/Breakfast/prata.png'),
+          key: 'Nasi Lemak', 
+          title:"Nasi Lemak",
+          image: require('../assets/WestFood/nasilemak.jpg'),
           text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled',
-          video: 'RSzITFbOtpQ',
+          video: 'KTK2lCyg0-k',
           rating: 4,
           status: 'Halal'
         }, 
         {
-          key: 'Chew', 
-          title:"hi",
+          key: 'Fried Kway Teow Mee', 
+          title:"Fried Kway Teow Mee (Ghee Huat)",
+          image: require('../assets/WestFood/teow.jpg'),
+          text:'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+          rating: 3,
+          status: 'Halal'
+        }, 
+        {
+          key: 'Chicken Wing',
+          title:"Chicken Wing (Chong Pang Huat)",
+          image: require('../assets/WestFood/chickw.jpg'),
+          text:'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+          rating: 3,
+          status: 'Halal'
+        }, 
+        {
+          key: 'Hokkien Prawn Mee',
+          title:"Hokkien Prawn Mee (Ho Huat Fried Hokkien Prawn Mee)",
+          image: require('../assets/WestFood/hmee.jpg'),
+          text:'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+          rating: 3,
+          status: 'All'
+        },
+        {
+          key: 'Ngoh Hiang', 
+          title:"Ngoh Hiang (Xin Sheng Gor Hiong Prawn Cracker)",
+          image: require('../assets/WestFood/ngoh.jpg'),
           text:'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
           rating: 3,
           status: 'All'
         }, 
-        {key: 'Chicken'}, {key: '4',status: 'All'},
-        {key: '1', title:"hi",status: 'All'}, 
-        {key: '2', status: 'All'}, 
-        {key: '3', status: 'All'}, 
-        {key: '3', status: 'All'}, 
+        {
+          key: 'Fish Porridge', 
+          title:"Fish Porridge (Lin Ji Yu Tang Yu Zhou 林記鱼汤鱼粥",
+          image: require('../assets/WestFood/porriage.jpg'),
+          text:'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+          rating: 3,
+          status: 'All'
+        },
+        {
+          key: 'Sambal Stingray', 
+          title:"Sambal Stingray (Lian Yi BBQ Seafood)",
+          image: require('../assets/WestFood/ray.jpg'),
+          text:'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+          rating: 3,
+          status: 'All'
+        },
+        {
+          key: 'Black Carrot Cake', 
+          title:"Black Carrot Cake (Boon Lay Fried Carrot Cake & Kway Teow Mee)",
+          image: require('../assets/WestFood/cake.jpg'),
+          text:'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+          rating: 3,
+          status: 'All'
+        },
+        {
+          key: 'Boneless Duck Noodles', 
+          title:"Boneless Duck Noodles (Heng Huat)",
+          image: require('../assets/WestFood/duck.jpg'),
+          text:'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+          rating: 3,
+          status: 'All'
+        },
+        {
+          key: 'Satay', 
+          title:"Satay (Boon Lay Satay)",
+          image: require('../assets/WestFood/satay.jpg'),
+          text:'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+          rating: 3,
+          status: 'All'
+        },
           ];
---
+++ array/dataCentre.js
@@ -0,0 +1,76 @@
+export 
+    const dataList = [
+        {
+            key: 'Teban Gardens Road Market and Food Centre',
+            title: 'Teban Gardens Road Market and Food Centre',
+            image: require('../assets/WestHawker/37aTeban.png'),
+            add: 'Blk 37A Teban Gardens Road',
+            time: '6am - 5am',
+            
+        },
+        {
+            key: 'Block 44 Holland Drive',
+            title: 'Block 44 Holland Drive',
+            image: require('../assets/WestHawker/44HollandDrive.jpg'),
+        },
+        {
+            key: 'Block 254 Jurong East Street 24',
+            title: 'Block 254 Jurong East Street 24',
+            image: require('../assets/WestHawker/254JEs24.jpg'),
+        },
+        {
+            key: 'Block 347 Jurong East Ave 1',
+            title: 'Block 347 Jurong East Ave 1',
+            image: require('../assets/WestHawker/347JEA1.png'),
+        },
+        {
+            key: 'Block 353 Clementi Ave 2',
+            title: 'Block 353 Clementi Ave 2',
+            image: require('../assets/WestHawker/353ClementiA2.png'),
+        },
+        {
+            key: 'Block 448 Clementi Ave 3',
+            title: 'Block 448 Clementi Ave 3',
+            image: require('../assets/WestHawker/448ClementiA3.png'),
+        },
+        {
+            key: 'Block 503 West Coast Drive',
+            title: 'Block 503 West Coast Drive',
+            image: require('../assets/WestHawker/503WCD.png'),
+        },
+        {
+            key: 'Block 505 Jurong West Street 52',
+            title: 'Block 505 Jurong West Street 52',
+            image: require('../assets/WestHawker/505JWS52.png'),
+        },
+        {
+            key: 'Block 726 Clementi West Street 2',
+            title: 'Block 726 Clementi West Street 2',
+            image: require('../assets/WestHawker/726ClementiWS2.png'),
+        },
+        {
+            key: 'Boon Lay Place Market and Food Village',
+            title: 'Boon Lay Place Market and Food Village',
+            image: require('../assets/WestHawker/BLM.jpg'),
+            add: '221A Boon Lay Pl, Singapore 641221',
+            time:'24 hours',
+            phone:'6225 5632'
+        },
+        {
+            key: 'Bukit Panjang Hawker Centre & Market',
+            title: 'Bukit Panjang Hawker Centre & Market',
+            image: require('../assets/WestHawker/BPHC.jpg'),
+        },
+        {
+            key: 'Holland Village Market & Food Centre',
+            title: 'Holland Village Market & Food Centre',
+            image: require('../assets/WestHawker/HVMFC.jpg'),
+        },
+        {
+            key: 'Taman Jurong Market & Food Centre',
+            title: 'Taman Jurong Market & Food Centre',
+            image: require('../assets/WestHawker/TJMFC.jpg'),
+        }
+    ];
+
+    
