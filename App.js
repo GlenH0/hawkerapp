5feeb292519d8a375c0d7e6c4d961d8a892908f7
Glen Ho <47803471+GlenH0@users.noreply.github.com>
2021-01-11 21:27:15 +0800

    13

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ const getFonts = () => Font.loadAsync({
     'jetbrains-ital': require('./assets/fonts/JetBrainsMono-Italic-VariableFont_wght.ttf'),
     'jetbrains-var': require('./assets/fonts/JetBrainsMono-VariableFont_wght.ttf'),
     'latoR': require('./assets/fonts/Lato-Regular.ttf'),
+    'latoB': require('./assets/fonts/Lato-Bold.ttf'),
   });
 
 
