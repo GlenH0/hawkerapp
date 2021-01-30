b7c5b2733c4449123a5c2d2ff598e61391acdcbc
Glen Ho <47803471+GlenH0@users.noreply.github.com>
2021-01-31 04:09:31 +0800

    15

---
+++ config.js
@@ -0,0 +1,25 @@
+// import * as React from 'react'
+// import App from './App'
+// import Firebase from '@react-native-firebase/app';
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCa319hDCxYOz6ZHZxh2dR0hgcXWiZQRjs",
+//     authDomain: "hawkerapp-6c26a.firebaseapp.com",
+//     projectId: "hawkerapp-6c26a",
+//     storageBucket: "hawkerapp-6c26a.appspot.com",
+//     messagingSenderId: "188233289575",
+//     appId: "1:188233289575:web:4f4c88a0e5f7a336d129c2"
+//   };
+
+
+//   if(!firebaseConfig.apps.length){
+//     Firebase.initializeApp(firebaseConfig);  
+//   }
+//   export const {Firebase}
+  
+//   function Setup(){
+//       return <App/>
+//   }
+
+//   export default Setup;
+
