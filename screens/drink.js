c3146bd582441f316812eec84115233bf8c41033
Glen Ho <47803471+GlenH0@users.noreply.github.com>
2021-02-06 03:38:54 +0800

    20

--- screens/drink.js
+++ screens/drink.js
@@ -1,6 +1,6 @@
 import firebase from '../firebase/fb'
-import React, {Component} from 'react';
-import {View,Text, FlatList, Dimensions, TouchableOpacity,Image, StyleSheet, ScrollView} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, FlatList, Dimensions, TouchableOpacity, Image, StyleSheet, ScrollView } from 'react-native';
 
 import { shuffle } from "lodash";
 import { Searchbar } from 'react-native-paper';
@@ -11,46 +11,46 @@ import _ from 'lodash'
 const numColumns = 2
 const WIDTH = Dimensions.get("window").width;
 
-export default class App extends Component{
+export default class App extends Component {
 
- constructor(props){
-   super(props);
-   this.state={ 
-    list:[],
-    searchText: '',
-    check: false
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+      searchText: '',
+      check: false
+    }
+
   }
-  
-}
 
- _renderItem = ({item, index}) => {
-     
-   return (
-     <View style={{flex:1}}>
-      
-       <View>
-         <TouchableOpacity style={styles.itemStyle} onPress={() => this.props.navigation.navigate('break', item)}>
-           <Image style={styles.img} source={{uri: item.image}}/>
-         </TouchableOpacity>
-       </View>
+  _renderItem = ({ item, index }) => {
 
-       <Text numberOfLines={1} style={{paddingLeft:10}}>{item.title}</Text>
-       <Image style={{marginLeft:9}} source={item['rating']} />
+    return (
+      <View style={{ flex: 1 }}>
 
-     </View>
-   )
-  
- }
+        <View>
+          <TouchableOpacity style={styles.itemStyle} onPress={() => this.props.navigation.navigate('break', item)}>
+            <Image style={styles.img} source={{ uri: item.image }} />
+          </TouchableOpacity>
+        </View>
 
-  componentDidMount(){
-    firebase.database().ref('drink').on('value', (snapshot) =>{
+        <Text numberOfLines={1} style={{ paddingLeft: 10 }}>{item.title}</Text>
+        <Image style={{ marginLeft: 9 }} source={item['rating']} />
+
+      </View>
+    )
+
+  }
+
+  componentDidMount() {
+    firebase.database().ref('drink').on('value', (snapshot) => {
       var li = []
-      snapshot.forEach((child)=>{
-       if(child.val().food_id == "1"){
-         li.push({
+      snapshot.forEach((child) => {
+        if (child.val().food_id == "1") {
+          li.push({
             title: child.val().title,
-            image:child.val().image,
-            image2:child.val().image2,
+            image: child.val().image,
+            image2: child.val().image2,
             video: child.val().video,
             subpage: child.val().subpage,
             subpageadd: child.val().subpageadd,
@@ -59,193 +59,206 @@ export default class App extends Component{
             subpagelat: child.val().subpagelat,
             subpagelong: child.val().subpagelong,
             subpagephone: child.val().subpagephone,
-            rating: child.val().rating, 
+            rating: child.val().rating,
             type: child.val().type,
-            foodtype: child.val().foodtype            
+            foodtype: child.val().foodtype
           })
-       }
+        }
+      })
+      this.setState({ list: li, inMemory: li })
     })
-   this.setState({list:li, inMemory: li})
-  })
- }
-
- componentWillUnmount() {
-   this._isMounted = false;
- }
-
-handleSearch = (text) => {
- const filter = this.state.inMemory.filter(
-   list => {
-     let title = list.title.toLowerCase()
-     let search = text.toLowerCase()
-
-     return title.indexOf(search) > -1
-   }
- )
- this.setState({list: filter, searchText:text})
-}
-
-handleFilter = () => {
-  this.setState({check: true})
-  if(this.state.check == true){
-    this.setState({list: this.state.inMemory, check: false})
   }
-}
 
-handleHalal = () =>{
-    this.setState({list: this.state.inMemory.filter(x => x.type === 'halal' && x.foodtype === 'noodle')})
-    console.log(this.state.check)   
-}
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
 
-handleFoodType = () =>{  
-    // this.setState({list: this.state.inMemory, check: false})
-      this.setState({list: this.state.inMemory.filter(x => x.foodtype === 'noodle')})
-      console.log(this.state.check)
-}
+  handleSearch = (text) => {
+    const filter = this.state.inMemory.filter(
+      list => {
+        let title = list.title.toLowerCase()
+        let search = text.toLowerCase()
 
-handleFoodTypeW = () =>{  
-  // this.setState({list: this.state.inMemory, check: false})
-    this.setState({list: this.state.inMemory.filter(x => x.foodtype === 'western')})
+        return title.indexOf(search) > -1
+      }
+    )
+    this.setState({ list: filter, searchText: text })
+  }
+
+  handleFilter = () => {
+    this.setState({ check: true })
+    if (this.state.check == true) {
+      this.setState({ list: this.state.inMemory, check: false })
+    }
+  }
+
+  handleHalal = () => {
+    this.setState({ list: this.state.inMemory.filter(x => x.type === 'halal' && x.foodtype === 'noodle') })
     console.log(this.state.check)
-}
- 
- render(){
-  return(
-    <View style={styles.container}>
+  }
 
-         <View style={{justifyContent:'center', alignItems:'center', paddingTop: 10}}>
-          <View style={{width: '95%'}}>     
+  handleFoodType = () => {
+    // this.setState({list: this.state.inMemory, check: false})
+    this.setState({ list: this.state.inMemory.filter(x => x.foodtype === 'noodle') })
+    console.log(this.state.check)
+  }
+
+  handleFoodTypeW = () => {
+    // this.setState({list: this.state.inMemory, check: false})
+    this.setState({ list: this.state.inMemory.filter(x => x.foodtype === 'western') })
+    console.log(this.state.check)
+  }
+
+  render() {
+    const { navigation } = this.props
+    return (
+      <View style={styles.container}>
+
+        <View style={{ justifyContent: 'center', alignItems: 'center', paddingTop: 10 }}>
+          <View style={{ width: '95%' }}>
             {
               renderIf(this.state.check == false)(
                 <Searchbar
-              placeholder="What's in mind today?"
-              onChangeText={(text)=>this.handleSearch(text)}
-              value={this.state.searchText}
-            />
+                  placeholder="What's in mind today?"
+                  onChangeText={(text) => this.handleSearch(text)}
+                  value={this.state.searchText}
+                />
               )
             }
           </View>
-         </View>
+        </View>
 
-         {renderIf(this.state.searchText == '')(
-            <View>
-              <Text style={{paddingLeft: 10, paddingTop: 10, fontFamily:'latoR', color:'#808080'}}>Suggested filters:</Text>
-           <ScrollView horizontal={true} style={{flexDirection: 'row'}}>
-            <TouchableOpacity
+        {renderIf(this.state.searchText == '')(
+          <View>
+            <Text style={{ paddingLeft: 10, paddingTop: 10, fontFamily: 'latoR', color: '#808080' }}>Suggested filters:</Text>
+            <ScrollView horizontal={true} style={{ flexDirection: 'row' }}>
+              <TouchableOpacity
                 onPress={this.handleFilter}
                 style={styles.btnTab}
               >
                 {renderIf(this.state.check == true)(
-                <Text style={{fontFamily:'latoR'}}>Back</Text>
+                  <Text style={{ fontFamily: 'latoR' }}>Back</Text>
                 )}
                 {renderIf(this.state.check == false)(
-                  <Text style={{fontFamily:'latoR'}}>View Filters</Text>
+                  <Text style={{ fontFamily: 'latoR' }}>View Filters</Text>
                 )}
               </TouchableOpacity>
 
+              {
+                renderIf(this.state.check == false)(
+                  <TouchableOpacity
+                    onPress={() => navigation.navigate('filterDessert')}
+                    style={styles.btnTab}
+                  >
+                    <Text style={{ fontFamily: 'latoR' }}>Random</Text>
+                  </TouchableOpacity>
+                )
+              }
+
               {renderIf(this.state.check == true)(
                 <TouchableOpacity
-                onPress={this.handleFoodType}
-                style={styles.btnTab}
-              >
-                <Text style={{fontFamily:'latoR'}}>Noodle</Text>
-              </TouchableOpacity>
+                  onPress={this.handleFoodType}
+                  style={styles.btnTab}
+                >
+                  <Text style={{ fontFamily: 'latoR' }}>Noodle</Text>
+                </TouchableOpacity>
               )}
               {renderIf(this.state.check == true)(
                 <TouchableOpacity
-                onPress={this.handleHalal}
-                style={styles.btnTab}
-              >
-                <Text style={{fontFamily:'latoR'}}>Halal</Text>
-              </TouchableOpacity>
+                  onPress={this.handleHalal}
+                  style={styles.btnTab}
+                >
+                  <Text style={{ fontFamily: 'latoR' }}>Halal</Text>
+                </TouchableOpacity>
               )}
-               {renderIf(this.state.check == true)(
+              {renderIf(this.state.check == true)(
                 <TouchableOpacity
-                onPress={this.handleHalal}
-                style={styles.btnTab}
-              >
-                <Text style={{fontFamily:'latoR'}}>Rice</Text>
-              </TouchableOpacity>
+                  onPress={this.handleHalal}
+                  style={styles.btnTab}
+                >
+                  <Text style={{ fontFamily: 'latoR' }}>Rice</Text>
+                </TouchableOpacity>
               )}
-               {renderIf(this.state.check == true)(
+              {renderIf(this.state.check == true)(
                 <TouchableOpacity
-                onPress={this.handleFoodTypeW}
-                style={styles.btnTab}
-              >
-                <Text style={{fontFamily:'latoR'}}>Western</Text>
-              </TouchableOpacity>
+                  onPress={this.handleFoodTypeW}
+                  style={styles.btnTab}
+                >
+                  <Text style={{ fontFamily: 'latoR' }}>Western</Text>
+                </TouchableOpacity>
               )}
-           </ScrollView>
+            </ScrollView>
           </View>
-          )}
+        )}
 
-          {renderIf(this.state.list == '')(
-            <View>
-              <Text style={{padding: 10}}>Ops! No results found</Text>
-            </View>
-          )}
+        {renderIf(this.state.list == '')(
+          <View>
+            <Text style={{ padding: 10 }}>Ops! No results found</Text>
+          </View>
+        )}
 
 
-       <FlatList style={{width:'100%'}}
+        <FlatList style={{ width: '100%' }}
           data={shuffle(this.state.list)}
-         renderItem={this._renderItem}
-         keyExtractor={(item, index) => index.toString() }
-         numColumns={numColumns}
-             />
-     </View>
-  )}
+          renderItem={this._renderItem}
+          keyExtractor={(item, index) => index.toString()}
+          numColumns={numColumns}
+        />
+      </View>
+    )
+  }
 }
- const styles = StyleSheet.create({
-    
-    container:{
-      flex: 1,
- 
-    },
-    itemStyle: {   
-      // shadow 
-      shadowOffset: { width: 12, height: 12 },
-      shadowColor: 'black',
-      shadowOpacity: 1,
-      elevation: 3,
-      backgroundColor : "#fff", 
+const styles = StyleSheet.create({
 
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: 100,
-      flex: 1,
-      margin: 10,
-      height: WIDTH/numColumns,    
-      borderRadius: 12
-      
-    },
-    itemText: {
-      color: 'white',
-      fontSize: 30
-    },
-    img:{
-      resizeMode:'cover',
-      width: '100%',
-      height:'100%',
-      overflow:'hidden',
-      borderRadius: 10,
-      
-    },
-    btnTab:{
-      width: 100,
-      flexDirection:'row',
-      padding: 10,
-      justifyContent:'center',
-      backgroundColor:'white',
-      borderRadius:30,
-      margin: 10,
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.23,
-      shadowRadius: 2.62,
+  container: {
+    flex: 1,
 
-      elevation: 4,
-    }
-  });
+  },
+  itemStyle: {
+    // shadow 
+    shadowOffset: { width: 12, height: 12 },
+    shadowColor: 'black',
+    shadowOpacity: 1,
+    elevation: 3,
+    backgroundColor: "#fff",
+
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 100,
+    flex: 1,
+    margin: 10,
+    height: WIDTH / numColumns,
+    borderRadius: 12
+
+  },
+  itemText: {
+    color: 'white',
+    fontSize: 30
+  },
+  img: {
+    resizeMode: 'cover',
+    width: '100%',
+    height: '100%',
+    overflow: 'hidden',
+    borderRadius: 10,
+
+  },
+  btnTab: {
+    width: 100,
+    flexDirection: 'row',
+    padding: 10,
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderRadius: 30,
+    margin: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+
+    elevation: 4,
+  }
+});
