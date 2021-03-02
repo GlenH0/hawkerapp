5c9f002ba94201f7149da267efea8ab02b0b3056
Glen Ho <47803471+GlenH0@users.noreply.github.com>
2021-03-03 03:28:39 +0800

    28

--- filter/filter.js
+++ filter/filter.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import { render } from 'react-dom';
-import { StyleSheet, Text, View, Dimensions, FlatList, TouchableOpacity, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, FlatList, TouchableOpacity, Button, Image, Animated } from 'react-native';
 import { dessertData } from '../array/dataDessert'
 import firebase from '../firebase/fb'
 import { shuffle } from "lodash";
+import { FontAwesome5 } from '@expo/vector-icons';
 
-const numColumns = 2
+const numColumns = 1
 const WIDTH = Dimensions.get("window").width;
 
 // const numOfFood = 3;
@@ -18,12 +19,26 @@ export default class FilterDessert extends React.Component {
       list: [],
       num:1,
       lastRefresh: Date(Date.now()).toString(),
+      animatedValue: new Animated.Value(0)
     };
     this.refreshScreen = this.refreshScreen.bind(this)
   }
 
   refreshScreen() {
     this.setState({ lastRefresh: Date(Date.now()).toString() })
+    Animated.sequence([
+      Animated.timing(this.state.animatedValue, {
+        toValue: 1,
+        duration: 700,
+        useNativeDriver: true,
+      }),
+      Animated.timing(this.state.animatedValue, {
+        toValue: 0,
+        duration: 0,
+        useNativeDriver: true,
+      })
+    ]).start()
+    this.flatListRef.scrollToOffset({ animated: true, offset: 0 });
   }
 
   _renderItem = ({ item, index }) => {
@@ -36,8 +51,8 @@ export default class FilterDessert extends React.Component {
           </TouchableOpacity>
         </View>
 
-        <Text numberOfLines={1} style={{ paddingLeft: 10 }}>{item.title}</Text>
-        <Image style={{ marginLeft: 9 }} source={item.rating} />
+        <Text numberOfLines={1} style={{ paddingLeft: 10, paddingBottom: 10 }}>{item.title}</Text>
+        {/* <Image style={{ marginLeft: 9 }} source={item.rating} /> */}
 
       </View>
     )
@@ -47,7 +62,7 @@ export default class FilterDessert extends React.Component {
     firebase.database().ref('foodBreak').once('value').then(snapshot=> {
       var li = []
       snapshot.forEach((child) => {
-        if (child.val().food_id == "1") {
+        if(child.val().food_id == "1"){
           li.push({
             title: child.val().title,
             image: child.val().image,
@@ -60,27 +75,29 @@ export default class FilterDessert extends React.Component {
             subpagelat: child.val().subpagelat,
             subpagelong: child.val().subpagelong,
             subpagephone: child.val().subpagephone,
-            rating: child.val().rating,
+            // rating: child.val().rating,
             type: child.val().type,
             foodtype: child.val().foodtype
           })
         }
       })
       this.setState({ list: li})
-    })
+    })  
   }
-
-
+  
   render() {
+    const rotation = this.state.animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg'],
+    })
+    const animatedStyle = {
+      transform: [
+        {rotate: rotation}
+      ]
+    }
     return (
       <View style={styles.container}>
-
-        {/* <Button
-          onPress={this.onPress}
-          title="Click here to filter"
-          color="#841584"
-          /> */}
-
+        <Text style={{padding:10}}> <Text style={{fontWeight:'bold'}}>10</Text> food items available</Text>
         <FlatList
         // this was previous halal filter
           // data={dessertData.filter(data => data.status === 'Halal').map(filteredData => (filteredData))}
@@ -88,10 +105,16 @@ export default class FilterDessert extends React.Component {
           renderItem={this._renderItem}
           keyExtractor={(item, index) => (index.toString())}
           numColumns={numColumns}  
+          ref={(ref) => { this.flatListRef = ref; }}
         />
-
-    <Button title="Roll" onPress={this.refreshScreen} />
-
+      {/* randomiser button */}
+      <View style={{justiftyContent:"center", alignItems:"center"}}>
+          <TouchableOpacity onPress={this.refreshScreen} style={styles.dice}>
+            <Animated.View style={animatedStyle}>
+              <FontAwesome5 name="dice-three" size={44} color="white" style={{ left:0, padding: 10, }}/>
+            </Animated.View>
+          </TouchableOpacity>
+      </View>
       </View>
     );
   }
@@ -117,7 +140,7 @@ const styles = StyleSheet.create({
     height: 100,
     flex: 1,
     margin: 10,
-    height: WIDTH / numColumns,
+    height: 250,
     borderRadius: 12
 
   },
@@ -143,5 +166,23 @@ const styles = StyleSheet.create({
   },
   btnTabActive: {
     backgroundColor: 'blue'
+  },
+  dice: {
+    position:'absolute', 
+    zIndex:100, 
+    bottom:30, 
+    right: 0, 
+    backgroundColor:'#ff5959',
+    borderBottomLeftRadius: 10, 
+    borderTopLeftRadius: 10, 
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+    
+    elevation: 9,
   }
 })
