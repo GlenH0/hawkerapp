5dc573389506bef95ef5860391fa6ff2f3714dd3
Glen Ho <47803471+GlenH0@users.noreply.github.com>
2021-03-11 23:55:14 +0800

    36

--- App.js
+++ App.js
@@ -10,10 +10,8 @@ const getFonts = () => Font.loadAsync({
     'jetbrains-var': require('./assets/fonts/JetBrainsMono-VariableFont_wght.ttf'),
     'latoR': require('./assets/fonts/Lato-Regular.ttf'),
     'latoB': require('./assets/fonts/Lato-Bold.ttf'),
-    'lob': require('./assets/fonts/Lobster-Regular.ttf'),
     'sat': require('./assets/fonts/PoetsenOne-Regular.ttf'),
-    'play': require('./assets/fonts/SpaceGrotesk-VariableFont_wght.ttf'),
-    'playB': require('./assets/fonts/MartelSans-Bold.ttf'),
+    'play': require('./assets/fonts/UmbaSoft-SCMediumDemo.otf'),
   });
 
 
